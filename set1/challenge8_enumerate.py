db7ba6ca7016818313252023931023bfb1c4632b
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-05-07 17:27:57 +0000

    Update challenge8_enumerate.py

--- set1/challenge8_enumerate.py
+++ set1/challenge8_enumerate.py
@@ -4,6 +4,10 @@
     will show all enumerated lines in the original ciphertext after being stripped of
     whitespace and trivial characters. 
 
+    This program is only useful for illuminating what happens when using the enumerate
+    function in the grand scheme of challenge 8. I put it in here for educational 
+    purposes.
+
 """
 
 def main():
