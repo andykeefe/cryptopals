da5a6196f47f949959c81ffcb780450ef67b180d
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-05-08 16:29:19 +0000

    Add files via upload

---
+++ set2/challenge9.py
@@ -0,0 +1,21 @@
+def pkcs7_padding(byte_plaintext: bytes, block_size: int = 16) -> bytes:
+    if block_size == 16:
+        padding = block_size - (len(byte_plaintext) & 15)
+    else:
+        padding = block_size - (len(byte_plaintext) % block_size)
+
+    return byte_plaintext + bytes([padding]) * padding
+
+    """ 
+
+
+
+    """
+
+
+if __name__ == '__main__':
+    plaintext = b'YELLOW SUBMARINE'
+    pad_plaintext = pkcs7_padding(plaintext, block_size = 48)
+
+    print(plaintext)
+    print(pad_plaintext)
