ac22e34b75e3d219f7b3ac8ab0088f0b9098a2b6
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-05-08 14:44:43 -0500

    Update challenge9v2.py

--- set2/challenge9v2.py
+++ set2/challenge9v2.py
@@ -1,9 +1,36 @@
 from Crypto.Util.Padding import pad, unpad
+"""
+
+    Padding with PyCryptodome is pretty simple, but takes away
+    a lot of the mathematical logic used in the first version of
+    this challenge. 
+
+    I didn't write this code on my native machine so I'm not able
+    to test it out yet, but I'm 95% sure it'll work when I access
+    a machine with PyCryptodome installed. 
+
+"""
 
 if __name__ == '__main__':
     plaintext = b'YELLOW SUBMARINE'
     pad_pt = pad(plaintext, 20, 'pkcs7')
+    
+    """
+
+        Pad plaintext to 20 bytes, pass in pkcs7 as padding scheme.
+        PKCS #7 is the default so this argument isn't necessary, but
+        I threw it in for illustrative purposes. 
+
+    """
+    
     unpad_pt = unpad(pad_pt, len(plaintext), 'pkcs7')
+
+    """ 
+
+        Unpad the padded text to the length of original plaintext, 16 bytes.
+        Again, PKCS #7 is the default but I passed it in for the sake of it.
+
+    """
    
     if unpad_pt == plaintext:
         print(f"{pad_pt=}")
