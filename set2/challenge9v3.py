d9e6523f0782b3567052c4df882da514d92e0a2b
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-05-09 10:50:20 -0500

    Cleaned up challenge9v3.py

--- set2/challenge9v3.py
+++ set2/challenge9v3.py
@@ -21,14 +21,12 @@ def unpad_pkcs7(padded: bytes, block_size: int) -> bytes:
 if __name__ == '__main__':
     
     plaintext = b'YELLOW SUBMARINE'
-    
     pad_pt = pad_pkcs7(plaintext, 20)
     unpad_pt = unpad_pkcs7(pad_pt, 16)
     
     if unpad_pt == plaintext:
         print(f"{pad_pt=}")
         print(f"{unpad_pt=}")
-        print("\nSuccess, PKCS7 implemented properly.\n")
-        
+        print("\nSuccess, PKCS7 implemented properly.\n")    
     else:
         print("Error. PKCS7 not implemented properly.")
