3cb79ca66843e98f59ac513b4b1b32ae8e780f8b
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-05-09 10:31:41 -0500

    Create challenge9v3.py

---
+++ set2/challenge9v3.py
@@ -0,0 +1,26 @@
+from Crypto.Util.Padding import pad, unpad
+
+def pad_pkcs7(pt: bytes, block_size: int) -> bytes:
+    padded_pt = pad(pt, block_size, style='pkcs7')
+    return padded_pt
+    
+    
+def unpad_pkcs7(padded: bytes, block_size: int) -> bytes:
+    unpadded_pt = unpad(padded, block_size, style='pkcs7')
+    return unpadded_pt
+
+  
+if __name__ == '__main__':
+    
+    plaintext = b'YELLOW SUBMARINE'
+    
+    pad_pt = pad_pkcs7(plaintext, 20)
+    unpad_pt = unpad_pkcs7(pad_pt, 16)
+    
+    if unpad_pt == plaintext:
+        print(f"{pad_pt=}")
+        print(f"{unpad_pt=}")
+        print("\nSuccess, PKCS7 implemented properly.\n")
+        
+    else:
+        print("Error. PKCS7 not implemented properly.")
