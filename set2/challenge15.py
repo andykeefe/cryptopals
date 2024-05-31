84144dc7b24a855ef557e5e522148a49ef7a4807
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-05-31 01:38:02 +0000

    Add files via upload

---
+++ set2/challenge15.py
@@ -0,0 +1,69 @@
+import sys
+sys.path.append("/home/andy/Documents/Cryptography/cryptopals")
+from set2.challenge09v2 import pad_pkcs7, unpad_pkcs7
+
+BLOCK_SIZE = 16
+
+def strip(string: bytes, block_size: int) -> bytes:
+    try:
+        if unpad_pkcs7(string, block_size) != target:
+            raise Exception("What the hell happened?????")
+            pass
+        
+        else:
+            print("-----------------------------------------------------------------------")
+
+            print(f"PASS: PKCS#7 implemented properly for {string}")
+
+            print("-----------------------------------------------------------------------")
+
+    except Exception as e:
+        print(f"{string}:", e)
+
+
+    """ 
+
+        We use a try and except statement to handle the exceptions that will
+        inevitably be raised when trying to strip the padding from bad_strings.
+        
+        First string passed in is base_string, which we know is good. However, if 
+        the stripped version is not our target, we raise an exception, pondering
+        how the hell that happened. If it is our target, we neatly print PASS and
+        the string we passed in.
+
+        The second and third strings are the nasty ones. The imported unpad_pkcs7
+        function uses the unpad function from the PyCryptoDome library, and we know
+        it's going to throw an exception for a ValueError, or something like that. 
+        To handle these exceptions, we print out the nasty string and the value of the
+        exception as e. 
+
+    """
+
+
+
+if __name__ == '__main__':
+    
+    target = b'ICE ICE BABY'
+
+    base_string = b"ICE ICE BABY\x04\x04\x04\x04"
+    bad_string1 = b"ICE ICE BABY\x05\x05\x05\x05"
+    bad_string2 = b"ICE ICE BABY\x01\x02\x03\x04"
+
+    """ 
+
+        Above are the test strings used to test if PKCS7 is implemented properly.
+        We, being geniuses, know that the base_string variable is correct, and we
+        know that bad_string1 and bad_string2 are incorrect. But we need to 
+        validate padding implementation in our code, so we have a target variable
+        that states the correct value we're trying to hit.
+
+    """
+    
+    print("")
+    strip(base_string, BLOCK_SIZE)
+    print("")
+    
+    strip(bad_string1, BLOCK_SIZE)
+    strip(bad_string2, BLOCK_SIZE)
+    print("")
+
