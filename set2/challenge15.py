7b30934f950c44f9646e2cb0dab0c2d7405b334f
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-06-03 12:52:47 -0500

    Update challenge15.py

--- set2/challenge15.py
+++ set2/challenge15.py
@@ -18,7 +18,21 @@ def strip(string: bytes, block_size: int) -> bytes:
     except Exception as e:
         print(f"{string}:", e)
 
-    """ 
+
+if __name__ == '__main__':
+    base_string = b"ICE ICE BABY\x04\x04\x04\x04"
+    bad_string1 = b"ICE ICE BABY\x05\x05\x05\x05"
+    bad_string2 = b"ICE ICE BABY\x01\x02\x03\x04"
+    
+    print("")
+    strip(base_string, BLOCK_SIZE)
+    print("")
+    
+    strip(bad_string1, BLOCK_SIZE)
+    strip(bad_string2, BLOCK_SIZE)
+    print("")
+
+""" 
 
         We use a try and except statement to handle the exceptions that will
         inevitably be raised when trying to strip the padding from bad_strings.
@@ -34,18 +48,4 @@ def strip(string: bytes, block_size: int) -> bytes:
         To handle these exceptions, we print out the nasty string and the value of the
         exception as e. 
 
-    """
-
-if __name__ == '__main__':
-    base_string = b"ICE ICE BABY\x04\x04\x04\x04"
-    bad_string1 = b"ICE ICE BABY\x05\x05\x05\x05"
-    bad_string2 = b"ICE ICE BABY\x01\x02\x03\x04"
-    
-    print("")
-    strip(base_string, BLOCK_SIZE)
-    print("")
-    
-    strip(bad_string1, BLOCK_SIZE)
-    strip(bad_string2, BLOCK_SIZE)
-    print("")
-
+"""
