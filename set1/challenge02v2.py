024ae1c6e532d0a20e8d046d72ec011a17bbdbd1
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-06-10 15:46:56 -0500

    Update challenge02v2.py

--- set1/challenge02v2.py
+++ set1/challenge02v2.py
@@ -5,15 +5,11 @@ def xor1(a, b):
     result = b''
     for byte_1, byte_2 in zip(a, b):
         result += bytes([byte_1 ^ byte_2])
-
     return bytes(result)
 
-
 if __name__ == '__main__':
-
    hex_string = xor1(a, b).hex()
    if hex_string == "746865206b696420646f6e277420706c6179":
        print("Successful XOR")
-
    else:
        print("Fail")
