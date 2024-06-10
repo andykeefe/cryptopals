55b92e0c51aa751666dad9685d0f4b883cb38c40
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-06-10 15:49:47 -0500

    Update challenge05.py

--- set1/challenge05.py
+++ set1/challenge05.py
@@ -48,14 +48,11 @@ if __name__ == '__main__':
     """
 
     ciphertext = vigenere_xor(b"ICE", plaintext)
-    
     success_cipher = bytes.fromhex("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
                                    "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
-
     if ciphertext == success_cipher:
         print("\nSUCCESS\n")
-
     else:
         print("\nFAIL\n")
-
+        
     print(ciphertext.hex() + "\n")
