3e0f32db2aad98fe2dc8f5f5092945343a8b4037
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-06-17 19:29:13 -0500

    Update challenge15.py

--- set2/challenge15.py
+++ set2/challenge15.py
@@ -10,17 +10,25 @@ def strip(string: bytes, block_size = int) -> bytes:
 
 
 if __name__ == '__main__':
-    base = b"ICE ICE BABY\x04\x04\x04\x04"
-    bad_str1 = b"ICE ICE BABY\x05\x05\x05\x05"
-    bad_str2 = b"ICE ICE BABY\x01\x02\x03\x04"
+    base = b"ICE ICE BABY\x04\x04\x04\x04"  # valid padding
+    bad_str1 = b"ICE ICE BABY\x05\x05\x05\x05"  # invalid padding
+    bad_str2 = b"ICE ICE BABY\x01\x02\x03\x04"  # invalid padding
 
     if strip(base, BLOCK_SIZE) != b"ICE ICE BABY":
         raise Exception("What the hell happened?")
     else:
         print(f"\n{base}: SUCCESSFUL PKCS#7 UNPADDING\n")
 
-    strip(bad_str1, BLOCK_SIZE)
+    strip(bad_str1, BLOCK_SIZE) 
     strip(bad_str2, BLOCK_SIZE)
+
+    """ 
+
+        If the strip() function is implemented properly, this should raise
+        an exception, printing the bad string and something like "Invalid PKCS#7
+        padding" for bad_str1 and bad_str2.  
+
+    """
 
     print("\n-----------------")
     print("ALL TESTS PASSED")
