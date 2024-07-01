faca9cff87de30756a093e8851ee1b84686042a3
Andrew Keefe <154836099+andykeefe@users.noreply.github.com>
2024-07-01 16:33:30 +0000

    Add files via upload

---
+++ set3/challenge19.py
@@ -0,0 +1,103 @@
+from base64 import b64decode
+from collections import Counter
+from os import urandom
+from challenge18 import aes_ctr
+
+import sys
+sys.path.append("/home/andy/Documents/Cryptography/cryptopals/set1")
+from challenge2v2 import xor1
+from challenge3v2 import score_text
+
+
+_key = urandom(16)
+
+texts = [aes_ctr(_key, b64decode(string)) for string in (
+    "SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==",
+    "Q29taW5nIHdpdGggdml2aWQgZmFjZXM=",
+    "RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==",
+    "RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=",
+    "SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk",
+    "T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+    "T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=",
+    "UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+    "QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=",
+    "T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl",
+    "VG8gcGxlYXNlIGEgY29tcGFuaW9u",
+    "QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==",
+    "QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=",
+    "QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==",
+    "QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=",
+    "QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+    "VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==",
+    "SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==",
+    "SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==",
+    "VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==",
+    "V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==",
+    "V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==",
+    "U2hlIHJvZGUgdG8gaGFycmllcnM/",
+    "VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=",
+    "QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=",
+    "VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=",
+    "V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=",
+    "SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==",
+    "U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==",
+    "U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=",
+    "VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==",
+    "QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu",
+    "SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=",
+    "VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs",
+    "WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=",
+    "SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0",
+    "SW4gdGhlIGNhc3VhbCBjb21lZHk7",
+    "SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=",
+    "VHJhbnNmb3JtZWQgdXR0ZXJseTo=",
+    "QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4="
+)]
+
+
+def find_keystream(ct) -> bytes:
+    len_ks = max(len(text) for text in ct)
+    byte_values = []
+
+    for i in range(len_ks):
+        ct_bytes = b''.join(bytes([text[i]]) for text in ct if i < len(text))
+        concatenated = len(ct_bytes)
+
+        best_score = float('inf')
+        best_byte = None
+
+        for j in range(256):
+            guess = xor1(ct_bytes, bytes([j]*concatenated))
+            score = score_text(guess)
+            if score < best_score:
+                best_score = score
+                best_byte = j
+
+        assert best_byte is not None
+        byte_values.append(best_byte)
+    return bytes(byte_values)
+
+
+if __name__ == '__main__':
+    c_sum = Counter(len(text) for text in texts)
+    print(f"Ciphertext length counts:{c_sum}")
+    print(f"Plaintexts with maximum length: {c_sum[max(c_sum)]} with length {max(c_sum)}")
+
+    prob_keystream = find_keystream(texts)
+    print(f"Guessed keystream: {prob_keystream}")
+    print("Potential candidates: ")
+
+    for text in texts:
+        pt_all = print(xor1(text, prob_keystream))
+
+    long_pt = b'he, too, has been changed in his turn'
+    long_ct = texts[-3]
+    deduced_ks = xor1(long_pt, long_ct)
+
+    print("-------------------------------------------")
+    print("Plaintext: ")
+
+    for text in texts:
+        print(xor1(text, deduced_ks).decode())
+
+
